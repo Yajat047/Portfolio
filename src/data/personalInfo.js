f143df94ce4bac1a1579c5a8de71e71456b994f1
Yajat Dureja <yajatdureja@gmail.com>
2025-11-11 15:34:53 +0530

    updates to personal info and added new BIMI logos

--- src/data/personalInfo.js
+++ src/data/personalInfo.js
@@ -5,7 +5,7 @@ export const personalInfo = {
   email: "yajatdureja@gmail.com",
   github: "https://github.com/Yajat047",
   linkedin: "https://linkedin.com/in/yajat47",
-  instagram: "https://instagram.com/Yajat47", // Update with your actual Instagram
+  instagram: "https://instagram.com/yajat.47", // Update with your actual Instagram
   resumeUrl: "https://drive.google.com/file/d/1VS16KxdO0xGCxTaWfuqnqBbPsSvt9NSw/view?usp=sharing",
   description: `▹ Full Stack Developer Intern at Coexivity
 ▹ Passionate about building full-stack applications and exploring Cloud technologies
