5fb18fceca7b0b082dfd7fb69ca8bfaa7b04ac26
Yajat Dureja <yajatdureja@gmail.com>
2025-11-26 19:14:49 +0530

    updated resume link

--- src/data/personalInfo.js
+++ src/data/personalInfo.js
@@ -6,7 +6,7 @@ export const personalInfo = {
   github: "https://github.com/Yajat047",
   linkedin: "https://linkedin.com/in/yajat47",
   instagram: "https://instagram.com/yajat.47", // Update with your actual Instagram
-  resumeUrl: "https://drive.google.com/file/d/1xtR4Qf31IL-L7BEJ9knBmCIN1ADFUWIY/view?usp=drive_link",
+  resumeUrl: "https://drive.google.com/file/d/1B6ICTJFOARO3v0zdh52tLlIJ25RPKgBS/view?usp=sharing",
   description: `▹ Full Stack Developer Intern at Coexivity
 ▹ Passionate about building full-stack applications and exploring Cloud technologies
 ▹ Actively seeking new opportunities to contribute and grow
